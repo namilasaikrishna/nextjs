dff2e5a1bff9efa4b3f586202d1600840db37131
Namilasaikrishna <nsai09.sk@gmail.com>
2022-12-28 04:39:35 -0800

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-function Home(){
-    return <h1>Home page</h1>
-}
+import Link from "next/link"
+import { useRouter } from "next/router"
 
+function Home(){
+    const router = useRouter()
+    const onClickHandler = () =>{
+        console.log('order placed successfully')
+        router.push('/products/')
+    }
+    return(
+        <div>
+        <h1>Home Page</h1>
+        <Link href="/blogs">Blog</Link>
+        <Link href="/products/">product</Link>
+        <button onClick={onClickHandler}>Place Order</button>
+        </div> 
+    )
+    
+}
 export default Home
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,9 +1,12 @@
-function Product(){
+import Link from "next/link"
+
+function Product({productId=100}){
     return(
         <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <Link href="/products/1" replace><li>Product 1</li></Link>
+        <Link href="/products/2" replace><li>Product 2</li></Link>
+        <Link href="/products/3" replace><li>Product 3</li></Link>
+        <Link href={`/products/${productId}`}>product{productId}</Link>
         </ul>
     )
 }
